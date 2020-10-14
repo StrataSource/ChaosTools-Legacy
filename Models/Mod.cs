9393bbb95e404d9404f35fce0fe0a25945c60940
Tewan <tewan@spycesoftware.com>
2020-10-14 20:30:00 +0200

    (feat) Add basic MVVM

---
+++ Models/Mod.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SDKLauncher.Models
+{
+    public class Mod
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using SDKLauncher.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace SDKLauncher.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public ObservableCollection<Mod> Mods { get; set; }
+        public Mod CurrentMod { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Mods = new ObservableCollection<Mod>
+            {
+                new Mod() {
+                    Name = "Portal 2: Community Edition"
+                },
+                new Mod() {
+                    Name = "Portal: Revolution"
+                }
+            };
+            CurrentMod = Mods[0];
+        }
     }
 }
