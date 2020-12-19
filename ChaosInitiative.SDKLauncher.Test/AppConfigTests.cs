a8bf1b77e46fbb92c94a65facac013b4c0f50570
Tewan <tewan@spycesoftware.com>
2020-12-19 18:07:18 +0100

    feat: Add tests for new AppConfig functions and refactor tests to use those functions instead of doing it manually

--- ChaosInitiative.SDKLauncher.Test/AppConfigTests.cs
+++ ChaosInitiative.SDKLauncher.Test/AppConfigTests.cs
@@ -13,7 +13,6 @@ namespace ChaosInitiative.SDKLauncher.Test
         {
             AppConfig config = AppConfig.CreateDefaultConfig();
             Assert.That(config.DefaultProfileIndex, Is.Zero);
-            
             Assert.That(config.Profiles, Is.Not.Empty);
         }
 
@@ -26,13 +25,13 @@ namespace ChaosInitiative.SDKLauncher.Test
                 config.Save();
             }, Throws.Nothing);
             
-            Assert.That(File.Exists("config.json"), Is.True);
+            Assert.That(AppConfig.IsConfigSaved, Is.True);
         }
 
         [Test, Order(2)]
         public void TestLoadConfigWorks()
         {
-            Assume.That(File.Exists("config.json"), Is.True);
+            Assume.That(AppConfig.IsConfigSaved, Is.True, "File config.json must exist on disk to load config");
 
             Assert.That(() =>
             {
@@ -40,22 +39,49 @@ namespace ChaosInitiative.SDKLauncher.Test
                 Assert.That(config, Is.Not.Null);
             }, Throws.Nothing);
             
-            File.Delete("config.json");
+            AppConfig.DeleteSavedConfig();
         }
 
         [Test, Order(3)]
         public void TestLoadConfigFailsWhenConfigDoesNotExist()
         {
-            Assume.That(File.Exists("config.json"), Is.False);
+            Assume.That(File.Exists("config.json"), Is.False, "File config.json must not exist on disk");
             Assert.That(AppConfig.LoadConfig, Throws.Exception.TypeOf<FileNotFoundException>());
         }
         
         [Test, Order(4)]
         public void TestLoadConfigFailsWhenConfigIsCorrupted()
         {
-            Assume.That(File.Exists("config.json"), Is.False);
+            Assume.That(AppConfig.IsConfigSaved, Is.False, "File config.json must not exist on disk");
             File.WriteAllText("config.json", "nbdsjvkjfdsbjhvsbjhv");
             Assert.That(AppConfig.LoadConfig, Throws.Exception.TypeOf<JsonException>());
+            AppConfig.DeleteSavedConfig();
+        }
+
+        [Test, Order(5)]
+        public void TestLoadConfigOrDefaultCreatesDefaultWhenFileNotFound()
+        {
+            Assume.That(AppConfig.IsConfigSaved, Is.False, "File config.json must not exist on disk");
+            
+            AppConfig loadedConfig = null;
+            Assert.That(() => loadedConfig = AppConfig.LoadConfigOrCreateDefault(), Throws.Nothing);
+            AppConfig defaultConfig = AppConfig.CreateDefaultConfig();
+            
+            Assert.That(loadedConfig, Is.Not.Null);
+            Assert.That(loadedConfig.DefaultProfileIndex, Is.Zero);
+            Assert.That(loadedConfig.Profiles.Count, Is.EqualTo(defaultConfig.Profiles.Count));
+            
+            Assert.That(AppConfig.IsConfigSaved, Is.True);
+            AppConfig.DeleteSavedConfig();
+        }
+
+        [Test, Order(6)]
+        public void TestLoadConfigOrDefaultThrowsWhenJsonCorrupted()
+        {
+            Assume.That(AppConfig.IsConfigSaved, Is.False, "File config.json must not exist on disk");
+            File.WriteAllText("config.json", "nbdsjvkjfdsbjhvsbjhv");
+            Assert.That(AppConfig.LoadConfigOrCreateDefault, Throws.Exception.TypeOf<JsonException>());
+            AppConfig.DeleteSavedConfig();
         }
     }
 }
